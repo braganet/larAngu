0b84914c8525fd5a775d17ccaafda4ab712a01e7
Adão Braga <braganet@live.com>
2016-09-19 17:19:02 -0300

    correção de funções Controller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -5,7 +5,6 @@ namespace LarAngu\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Illuminate\Support\Facades\Input;
-use Illuminate\Support\Facades\Lang;
 use LarAngu\Client;
 use LarAngu\Http\Requests;
 
@@ -15,40 +14,50 @@ class ClientController extends Controller
     {
         return \LarAngu\Client::all();
     }
-
     public function store(Request $request)
     {
         // C Recebe os dados do cliente e manda para o banco
         return Client::create($request->all());
     }
-
     public function show($id)
     {   // R Selecionando campos no banco.
         return Client::find($id);
     }
-
-    public function update($id)
+    public function update(Request $request, $id)
     {
         // U atualizando dados
-        $cliente = Client::find($id);
 
-        $cliente->name = Input::get('name');
-        $cliente->responsible = Input::get('responsible');
-        $cliente->email = Input::get('email');
-        $cliente->phone = Input::get('phone');
-        $cliente->address = Input::get('address');
-        $cliente->obs = Input::get('obs');
+            $cliente = Client::find($id);
 
-        $cliente->save();
+            $cliente->name = Input::get('name');
+            $cliente->responsible = Input::get('responsible');
+            $cliente->email = Input::get('email');
+            $cliente->phone = Input::get('phone');
+            $cliente->address = Input::get('address');
+            $cliente->obs = Input::get('obs');
 
+            if (!$cliente->save()){
+                return response()->json("Não foi possivél atualizar os dados");
+            }else{
+
+                return response()->json("Dados Atualizados com Sucesso!");
+            }
+
+        //return response()->json($request->all());
         /*
          * Um erro: o campo: created_at está sendo atualizado juntamente.
          * */
     }
-
-
     public function destroy($id)
     {   // D Deleta o campo no banco.
-        Client::find($id)->delete();
+
+        if (!Client::find($id)->delete()){
+            return response()->json("? Não foi possivél apagar o registro");
+        }else{
+            return response()->json("Registro apagado com sucesso!");
+        }
+
+
     }
+
 }
