1d9a0f49b110c14b52b1f5cf527749ab82442a29
Adão Braga <braganet@live.com>
2016-09-20 10:11:58 -0300

    Atualizando o ClientController funo update e destroy

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -26,38 +26,25 @@ class ClientController extends Controller
     public function update(Request $request, $id)
     {
         // U atualizando dados
-
-            $cliente = Client::find($id);
-
-            $cliente->name = Input::get('name');
-            $cliente->responsible = Input::get('responsible');
-            $cliente->email = Input::get('email');
-            $cliente->phone = Input::get('phone');
-            $cliente->address = Input::get('address');
-            $cliente->obs = Input::get('obs');
-
-            if (!$cliente->save()){
-                return response()->json("Não foi possivél atualizar os dados");
-            }else{
-
-                return response()->json("Dados Atualizados com Sucesso!");
-            }
-
-        //return response()->json($request->all());
-        /*
-         * Um erro: o campo: created_at está sendo atualizado juntamente.
-         * */
+        try {
+            $cliente = Client::findorfail($id);
+            $updateNow = $cliente->update($request->all());
+            return response()->json("Dados Atualizados com Sucesso!");
+        } catch (\Exception $e) {
+            return response()->json("Não foi possivél atualizar os dados");
+        }
     }
+
     public function destroy($id)
     {   // D Deleta o campo no banco.
 
-        if (!Client::find($id)->delete()){
-            return response()->json("? Não foi possivél apagar o registro");
-        }else{
+        try{
+            $cliente = Client::findorfail($id);
+            $cliente->delete();
             return response()->json("Registro apagado com sucesso!");
+        }catch (\Exception $e){
+            return response()->json("? Não foi possivél apagar o registro");
         }
-
-
     }
 
 }
